f434c08fbe380340739fd552db8ffc0ac0ae9c18
Vlad <Vlad.Levyant@zaha-hadid.com>
2023-09-13 17:14:27 +0100

    Improved batch file

--- BuildDLLs.bat
+++ BuildDLLs.bat
@@ -1,6 +1,21 @@
+@echo off
+set arg0=%~0
 
+if "%arg0:~0,2%" == "C:" GOTO zcore
+if "%arg0:~0,2%" == "De" GOTO other
+
+:zcore
 call premake\premake5.exe vs2019
 
-start /d "%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin" MSBuild.exe C:\Users\Vlad.Levyant\source\repos\zspace_core\zSpace_core.sln /t:zSpace_core /p:Configuration="Release_DLL" /p:Platform="x64"
+start /d "%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin" MSBuild.exe %cd%\zSpace_core.sln /t:zSpace_core /p:Configuration="Release_DLL" /p:Platform="x64"
+pause
+GOTO end
 
-Pause
+:other
+
+call premake\premake5.exe --file=buildDeps.lua vs2019
+
+start /d "%programfiles(x86)%\Microsoft Visual Studio\2019\Professional\MSBuild\Current\Bin" MSBuild.exe %cd%\Dependencies\ZSPACE\zSpace_core.sln /t:zSpace_core /p:Configuration="Release_DLL" /p:Platform="x64"
+goto :end
+
+:end
